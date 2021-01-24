e3097461323bca1e302096c493d4ca67785e697f
leejongwook1 <leejongwook@naver.com>
2021-01-24 17:29:29 +0900

    load movie and css commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    //비동기, 데이터를 받아올때까지 기다리게
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    ); //데이터를 가져오는데 시간이 걸릴수 있기때문에 await으로 기다림, sort_by=rating 은 별점순으로 정렬
+    console.log(movies);
+
+    this.setState({
+      movies, //받아온 영화 데이터
+      isLoading: false,
+    }); //데이터를 state에 넣어주면 로딩이 끝나게 함
+  };
+
+  componentDidMount() {
+    this.getMovies();
+    // setTimeout(() => {                    로딩 시간을 설정
+    //   this.setState({ isLoading: false });
+    // }, 6000);
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader-text">Loading.....</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
